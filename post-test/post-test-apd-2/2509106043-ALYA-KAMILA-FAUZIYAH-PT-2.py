f88f3a8585ff37f9a39f1488b409a9ca521e36d7
yaralyar07 <alya.kamila2937@gmail.com>
2025-09-23 21:22:03 +0800

    second commit

---
+++ post-test/post-test-apd-2/2509106043-ALYA-KAMILA-FAUZIYAH-PT-2.py
@@ -0,0 +1,27 @@
+# Variabel
+nama = "Alya Kamila Fauziyah"                            # str
+umur = 18                                                # int
+tahun_lahir = 2007                                       # int
+tinggi_badan = 153.5                                     # float
+berat_badan = 45.6                                       # float
+is_mahasiswa = True                                      # bool
+is_menikah = False                                       # bool
+alamat = "Jl. A. Azis Samad Gang Kenari 1, Samarinda"    # str
+jumlah_saudara = 2                                       # int
+punya_kendaraan = True                                   # bool
+
+# Print Biodata
+print("=" * 30)
+print(" BIODATA DIRI")
+print("=" * 30)
+print("Nama             : " + nama)
+print("Umur             : " + str(umur) + " tahun")
+print("Tahun Lahir      : " + str(tahun_lahir))
+print("Tinggi Badan     : " + str(tinggi_badan) + " cm")
+print("Berat Badan      : " + str(berat_badan) + " kg")
+print("Status Mahasiswa : " + str(is_mahasiswa))
+print("Status Menikah   : " + str(is_menikah))
+print("Alamat           : " + alamat)
+print("Jumlah Saudara   : " + str(jumlah_saudara))
+print("Punya Kendaraan  : " + str(punya_kendaraan))
+print("=" * 30)
